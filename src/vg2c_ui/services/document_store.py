f2e0343df0cdb04e2007114092e08648a367335f
VectorLim <yeu.chuan.lim@intel.com>
2026-09-18 15:33:01 +0800

    api: remove redundant semantic parameter alias

--- src/vg2c_ui/services/document_store.py
+++ src/vg2c_ui/services/document_store.py
@@ -305,7 +305,6 @@ class DocumentStore:
         return SqlActionResponse(
             change=SemanticChangeRequest(
                 binding_id=change.binding_id,
-                parameter_id=change.binding_id,
                 value=change.value,
             ),
             model=sql_model_view(
