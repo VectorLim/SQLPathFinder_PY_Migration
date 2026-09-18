cc37f9fd77d9098a1470d70f2b084023022bfed9
VectorLim <yeu.chuan.lim@intel.com>
2026-09-18 16:16:33 +0800

    refactor(core): declare email semantic capabilities

--- src/vg2c/utilities/mail.py
+++ src/vg2c/utilities/mail.py
@@ -139,6 +139,8 @@ class MailService(EmitterUtility):
 
     @emittable(
         display_name="Send Email",
+        capabilities=("email",),
+        parameter_capabilities={"attachments": ("file-input",)},
         file_effects=(
             FileEffectDefinition(
                 "attachments",
