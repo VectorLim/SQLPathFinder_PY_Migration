2aabeb0f5d1b9cdd76a03c14d6a6a3d53b40068f
VectorLim <yeu.chuan.lim@intel.com>
2026-09-18 14:28:40 +0800

    feat: name pipeline semantic operations

--- src/vg2c/utilities/pipeline_context.py
+++ src/vg2c/utilities/pipeline_context.py
@@ -20,6 +20,7 @@ class PipelineContext(UtilitySpec):
         self.__dict__.update(utilities)
 
     @emittable(
+        display_name="Write File",
         file_effects=(FileEffectDefinition("write", "write", outputs=("path",)),)
     )
     def write_file(
@@ -40,6 +41,7 @@ class PipelineContext(UtilitySpec):
         return result
 
     @emittable(
+        display_name="Run Query",
         file_effects=(
             FileEffectDefinition(
                 "query",
@@ -101,6 +103,6 @@ class PipelineContext(UtilitySpec):
 
         self.csv_io.write(output, result, header=header)
 
-    @emittable(file_effects=())
+    @emittable(file_effects=(), visibility="internal")
     def eval_condition(self, lhs: str, op: str, rhs: str, *args: Any) -> bool:
         return self.macro.eval_condition(lhs, op, rhs)
