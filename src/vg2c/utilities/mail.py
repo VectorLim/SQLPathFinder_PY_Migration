6431b5836cb8327646da3942e87d71b0a2b62b82
VectorLim <yeu.chuan.lim@intel.com>
2026-09-18 16:42:15 +0800

    refactor(core): reuse file-effect attachment capability

--- src/vg2c/utilities/mail.py
+++ src/vg2c/utilities/mail.py
@@ -140,7 +140,6 @@ class MailService(EmitterUtility):
     @emittable(
         display_name="Send Email",
         capabilities=("email",),
-        parameter_capabilities={"attachments": ("file-input",)},
         file_effects=(
             FileEffectDefinition(
                 "attachments",
