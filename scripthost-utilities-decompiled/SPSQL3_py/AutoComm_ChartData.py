5bae6225769238943e03e840330c63c0c5376955
VectorLim <yeu.chuan.lim@intel.com>
2026-09-26 17:02:54 +0800

    refactor: use package-relative report imports

--- scripthost-utilities-decompiled/SPSQL3_py/AutoComm_ChartData.py
+++ scripthost-utilities-decompiled/SPSQL3_py/AutoComm_ChartData.py
@@ -35,12 +35,12 @@ import os
 import re
 import pickle
 import logging
-from PyGraphingMethods import Basic_Charts
-from PyGraphingMethods import Correlation_Plot
-from PyGraphingMethods import Carrier_Map
-from PyGraphingMethods import Wafer_Map
-from PyUtils import FindPattern
-from PyUtils import IsRectangle
+from .PyGraphingMethods import Basic_Charts
+from .PyGraphingMethods import Correlation_Plot
+from .PyGraphingMethods import Carrier_Map
+from .PyGraphingMethods import Wafer_Map
+from .PyUtils import FindPattern
+from .PyUtils import IsRectangle
 
 
 class AC_Chart_Data(object):
