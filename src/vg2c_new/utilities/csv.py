55f8f8a1f10008d9300ca61dd7ed4e3515758e93
VectorLim <yeu.chuan.lim@intel.com>
2026-09-26 17:09:04 +0800

    style: separate vendored imports

--- src/vg2c_new/utilities/csv.py
+++ src/vg2c_new/utilities/csv.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 
 import pandas as pd
 from SPSQL3_py.SPFLib.SPFUtilities.portable import get_file_delimiter
+
 from vg2c_new.paths import resolve_path, working_directory_for
 
 if TYPE_CHECKING:
